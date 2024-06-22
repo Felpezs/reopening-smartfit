ff974ebf89423bbbeefc3252635e73dabbe2bcbb
Felipe Eduardo <felipe.edfreire@gmail.com>
2024-06-22 12:05:40 -0300

    feat: Header component (#1)

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import Logo from "./Logo";
+
+const Header = () => {
+  return (
+    <header className="bg-darkGrey w-full h-24 flex items-center justify-center">
+      <Logo />
+    </header>
+  );
+};
+
+export default Header;
