317c5bbf8170e80e8a0ac4f3a155727fcdac5711
Felpezs <felipe.edfreire@gmail.com>
2024-06-22 14:34:20 -0300

    feat: adding page sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import Form from "./components/Form/Form";
 import Header from "./components/Header";
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col items-center">
       <Header />
-      <main></main>
-    </>
+      <main className="w-[80%] max-w-screen-lg my-20">
+        <section className="w-full">
+          <Form />
+        </section>
+      </main>
+    </div>
   );
 }
 
