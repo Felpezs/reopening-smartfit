53378c00e4c99d7e55b1b980df6369afae923aee
Felpezs <felipe.edfreire@gmail.com>
2024-06-28 18:29:11 -0300

    feat: finalized locations

--- src/components/Legend/Legend.tsx
+++ src/components/Legend/Legend.tsx
@@ -3,7 +3,7 @@ import { Legend as LegendProps } from "@/types/Legend";
 const Legend = ({ image, status }: LegendProps) => {
   return (
     <>
-      <img src={image} className="w-16  h-16  mx-auto mb-2" />
+      <img src={image} className="w-16 h-16 mx-auto mb-2" />
       <span className="text-black font-normal block text-center">{status}</span>
     </>
   );
