9e8753f817fe7c18ae7939b6b00d984019d52add
Felpezs <felipe.edfreire@gmail.com>
2024-06-28 18:28:52 -0300

    feat: card component

---
+++ src/components/Card/Cards.tsx
@@ -0,0 +1,37 @@
+import mapLegends from "@/utils/mapLegends";
+import Card from "./Card";
+import { useLocationsContext } from "@/context/LocationsContext";
+
+const Cards = () => {
+  const { locationsResponse } = useLocationsContext();
+
+  return (
+    <div className="flex flex-wrap justify-center gap-5">
+      {locationsResponse &&
+        locationsResponse.locations.map((location) => {
+          if (location.opened) {
+            return (
+              <Card
+                key={location.title}
+                isOpen={location.opened}
+                address={location.content}
+                name={location.title}
+                legends={mapLegends(location)}
+                schedules={location.schedules}
+              />
+            );
+          }
+          return (
+            <Card
+              key={location.title}
+              isOpen={location.opened}
+              address={location.content}
+              name={location.title}
+            />
+          );
+        })}
+    </div>
+  );
+};
+
+export default Cards;
