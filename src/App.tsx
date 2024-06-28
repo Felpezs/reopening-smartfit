39a6923676c443f5a6ddee5df91b988573da32f7
Felpezs <felipe.edfreire@gmail.com>
2024-06-27 22:12:05 -0300

    feat: sharing query provider context

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,25 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import Form from "./components/Form/Form";
 import Header from "./components/Header";
 import Legend from "./components/Legend/LegendContainer";
+import Cards from "./components/Card/Cards";
+import { LocationsProvider } from "./context/LocationsContext";
 
 function App() {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-      },
-    },
-  });
-
   return (
     <div className="flex flex-col items-center">
       <Header />
       <main className="w-[80%] max-w-screen-lg my-20 flex flex-col gap-10">
-        <section className="w-full">
-          <QueryClientProvider client={queryClient}>
+        <LocationsProvider>
+          <section className="w-full">
             <Form />
-          </QueryClientProvider>
-        </section>
-        <section className="w-full font-bold">
-          <Legend />
-        </section>
+          </section>
+          <section className="w-full font-bold">
+            <Legend />
+          </section>
+          <section>
+            <Cards />
+          </section>
+        </LocationsProvider>
       </main>
     </div>
   );
